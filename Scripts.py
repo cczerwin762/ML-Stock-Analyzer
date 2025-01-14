a882a55a57b81d6493d467def1b99b4ee8bed108
Chris Czerwinski <cczerwinski123@gmail.com>
2025-01-13 20:47:28 -0700

    Not working. Fix scripts

--- Scripts.py
+++ Scripts.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import datetime
 from Helper import *
+import yfinance as yf
 import re
 from WebScraper import *
 '''
@@ -14,12 +15,24 @@ url = 'https://stockanalysis.com/stocks/sector/technology/'
 attr = 'href'
 filter = '/stocks/'
 
+
 ws = WebScraper(url,attr,filter)
 ticks = []
 ws.scrapeToList(ticks)
-print('Script start')
+i = 0
+end = len(ticks)
+while i < end :
+    stock = yf.Ticker(ticks[i])
+    try:
+        tmp = stock.info
+    except:
+        del ticks[i]
+        end-=1
+        continue
+    i+=1
 print(ticks)
 #clean these tickers and helper file
+
 
 
 
